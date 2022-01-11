9829e4f9da2b0219d3f57f7fbb965f80d51b947a
Wizos <wizos@users.noreply.github.com>
2022-01-11 15:23:44 +0800

    Update repositories.gradle.kts

--- repositories.gradle.kts
+++ repositories.gradle.kts
@@ -10,3 +10,9 @@ repositories {
     gradlePluginPortal()
     maven(url = "https://jitpack.io")
 }
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
